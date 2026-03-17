31e8e854222cf8a3b084a3bfbaca85b59d31213b
marionnejizella-ai <marionnejizella@gmail.com>
2026-03-17 09:32:32 +0400

    hello.py

--- Exercise 9 - Assessment/hello.py
+++ Exercise 9 - Assessment/hello.py
@@ -1,3 +1,11 @@
 #Exercise 9: Hello
 #Marionne Jizella E. Centeno
-print("Hello")
+
+def hello():
+    print("Hello") # Fill in this blank to print "Hello" to the console
+
+def main():
+    hello() # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+    main()
